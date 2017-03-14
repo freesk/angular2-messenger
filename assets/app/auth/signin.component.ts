84d2a3d28797845ac26143802cb8f63310fdddf4
Dmitry <dmitr.kurbatov@gmail.com>
2017-03-14 12:24:30 +0200

    signin front-end

--- assets/app/auth/signin.component.ts
+++ assets/app/auth/signin.component.ts
@@ -1,9 +1,28 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html'
 })
-export class SigninComponent {
+export class SigninComponent implements OnInit {
+  myForm: FormGroup;
+
+  onSubmit() {
+    console.log(this.myForm);
+  }
+
+  // On init
+  ngOnInit() {
+    this.myForm = new FormGroup({
+      // Setting up controls of the form
+      email: new FormControl(null, [
+        Validators.required,
+        // Regex
+        Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")
+      ]),
+      password: new FormControl(null, Validators.required)
+    });
+  }
 
 }
